2b8b06aab009970e9470be5684b26436f136abdc
Giles Davies <giles.j.davies@hotmail.com>
2020-07-30 16:50:05 +0100

    Tidied arg handling

--- foianalyser.py
+++ foianalyser.py
@@ -8,24 +8,26 @@ import sys
 # 1: Word doc to analyze
 # 2: FOI Profile (Victim, Suspect, Informant)
 
-if len(sys.argv) <= 1:
-    raise ValueError('Please provide the following arguments: word file and FOI profile [victim, suspect, informant]')
-doc = sys.argv[1]
-foiprofile = sys.argv[2]
+if len(sys.argv) == 3:
+    doc = sys.argv[1]
+    foiprofile = sys.argv[2]
+else:
+    exampleusage = sys.argv[0] + " worddoc.docx" + " victim"
+    raise ValueError('Arguments required: word doc and FOI profile e.g.: ' + exampleusage)
 
 print("\n===============================================")
 print("\nPreparing to analyse:", doc)
 print("FOI profile:", foiprofile)
-print("===============================================")
+print("\n===============================================")
 
 document = Document(doc)
 
+print("\n===============================================")
 print("\nDocument properties for", doc)
-print("===============================================")
 print("Author:", document.core_properties.author)
 print("Last Modified By:", document.core_properties.last_modified_by)
 print("Date:", document.core_properties.modified)
-print("===============================================\n")
+print("\n===============================================")
 
 paras = document.paragraphs
 
@@ -33,10 +35,10 @@ doctext = ""
 for i in paras:
     doctext += i.text
 
-print("Extracted text from document")
-print("----------------------------------------\n")
+print("\n===============================================")
+print("Extracted text from", doc)
 print(doctext)
-print("----------------------------------------\n")
+print("\n===============================================")
 
 engine = AnalyzerEngine()
 
